03ac26666998a8bc4a023798c424b8dddade3b4f
unknown <passionateartist2004@gmail.com>
2024-11-29 13:02:47 +0530

    app

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, send_file
 from TextGen import GenText2
 from pptMake import GenPPT
 from PPTPicker import PickPPT
+import os 
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -16,13 +17,14 @@ def generate_ppt():
     '''file="modern4.pptx"
     slideInfo={"lenth":56,"p":1,"s":2,"m":4,"b":3}'''
     ppt=file
+    outputFile=os.path.join(os.getcwd(), "temp.pptx")
     if not user_input:
         return jsonify({"error": "No user input provided."}), 400
     details=GenText2(user_input,slideInfo["length"],25)
     
-    genrated=GenPPT(ppt,"temp.pptx",details,slideInfo)
+    genrated=GenPPT(ppt,outputFile,details,slideInfo)
     if genrated:
-        return send_file("temp.pptx", as_attachment=True, download_name="updated_pitch_deck.pptx")
+        return send_file(outputFile, as_attachment=True, download_name="updated_pitch_deck.pptx")
     else:
         return False
 if __name__ == '__main__':
